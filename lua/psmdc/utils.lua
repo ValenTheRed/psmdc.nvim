f17c18311ff3ceb30a734945797bf88238951444
Pranjal Soni <soni.pranjals17@gmail.com>
2022-04-12 11:46:38 +0530

    Feat(utils): blend()

--- lua/psmdc/utils.lua
+++ lua/psmdc/utils.lua
@@ -1,4 +1,18 @@
 local M = {}
+hsluv = require("psmdc.hsluv")
+math = require("math")
+
+-- From tokyonight.util
+---@param fg string foreground color
+---@param bg string background color
+---@param alpha number number between 0 and 1. 0 results in bg, 1 results in fg
+function M.blend(fg, bg, alpha)
+  local bg, fg = hsluv.hex_to_rgb(bg), hsluv.hex_to_rgb(fg)
+  local blendChannel = function(i)
+    return alpha * fg[i] + (1 - alpha) * bg[i]
+  end
+  return hsluv.rgb_to_hex {blendChannel(1), blendChannel(2), blendChannel(3)}
+end
 
 function M.set_highlight(grp, args)
   vim.api.nvim_command(string.format(
