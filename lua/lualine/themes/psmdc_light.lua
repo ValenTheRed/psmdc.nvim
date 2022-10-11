d3bf91d9d386f627799b3213036c4dd6899b320a
Pranjal Soni <soni.pranjals17@gmail.com>
2022-10-11 15:13:09 +0530

    Add light colorscheme

---
+++ lua/lualine/themes/psmdc_light.lua
@@ -0,0 +1,34 @@
+local c = require("psmdc.colors").light
+
+local psmdc = {}
+
+psmdc.normal = {
+  a = { fg=c.grp.bg, bg=c.clr.purple },
+  b = { fg=c.grp.surfacefg_darker, bg=c.grp.surface_darker},
+  c = { fg=c.grp.surfacefg, bg=c.grp.surface },
+}
+
+
+psmdc.insert = {
+  a = { fg=c.grp.bg, bg=c.clr.pink },
+}
+
+psmdc.command = {
+  a = { fg=c.grp.bg, bg=c.clr.blue },
+}
+
+psmdc.visual = {
+  a = { fg=c.grp.bg, bg=c.clr.yellow },
+}
+
+psmdc.replace = {
+  a = { fg=c.grp.bg, bg=c.clr.orange },
+}
+
+psmdc.inactive = {
+  a = { fg=c.grp.surfacefg_darker, bg=c.grp.surface_darker },
+  b = { fg=c.grp.surfacefg_darker, bg=c.grp.surface_darker },
+  c = { fg=c.grp.surfacefg_darker, bg=c.grp.surface_darker },
+}
+
+return psmdc
--- lua/psmdc/colors.lua
+++ lua/psmdc/colors.lua
@@ -75,4 +75,40 @@ M.darker.grp = {
   surfacefg_darker = hsluv {256, 0, 70},
 }
 
+M.light = {}
+M.light.clr = {
+  white  = hsluv {0, 0, 100},
+  black  = hsluv {0, 0, 0},
+  gray   = hsluv {0, 0, 54},
+  yellow = hsluv {50, 100, 65},
+  orange = hsluv {20, 88, 64.5},
+  red    = hsluv {7, 91.2, 54.1},
+  pink   = hsluv {337, 82, 60},
+  purple = hsluv {288, 100,  45},
+  blue   = hsluv {259, 100, 45},
+  cyan   = hsluv {224, 100, 70},
+  green  = hsluv {116, 100, 65},
+
+  green2 = hsluv {115, 59, 58},
+  violet = hsluv {314, 34, 61},
+  brown  = hsluv {21.5, 76, 55.3},
+
+  lightorange = hsluv {14, 88, 64.5},
+  lightpink   = hsluv {337, 82, 64},
+  lightblue   = hsluv {259, 100, 70},
+  lightyellow = hsluv {52, 100, 75},
+}
+M.light.grp = {
+  bg               = hsluv {305, 55, 95},
+  fg               = hsluv {256, 0, 25},
+  comments         = hsluv {256, 0, 60},
+  nontext          = hsluv {256, 0, 80},
+  selection        = hsluv {285, 28, 82},
+  cursor_line      = hsluv {305, 55, 92.5},
+  surface          = hsluv {305, 55, 83},
+  surface_darker   = hsluv {305, 55, 90},
+  surfacefg        = hsluv {256, 0, 25},
+  surfacefg_darker = hsluv {256, 0, 30},
+}
+
 return M
