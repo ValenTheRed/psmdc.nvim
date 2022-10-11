36639b52b57b4d08a0da64afac844d16ce457eac
Pranjal Soni <soni.pranjals17@gmail.com>
2022-10-11 15:31:58 +0530

    lualine: section b's foreground colour matches current mode colour

--- lua/lualine/themes/psmdc_dark.lua
+++ lua/lualine/themes/psmdc_dark.lua
@@ -4,24 +4,28 @@ local psmdc = {}
 
 psmdc.normal = {
   a = { fg=c.grp.bg, bg=c.clr.purple },
-  b = { fg=c.grp.surfacefg_darker, bg=c.grp.surface_darker },
+  b = { fg=c.clr.purple, bg=c.grp.surface_darker },
   c = { fg=c.grp.surfacefg, bg=c.grp.surface },
 }
 
 psmdc.insert = {
   a = { fg=c.grp.bg, bg=c.clr.pink },
+  b = { fg=c.clr.pink, bg=c.grp.surface_darker},
 }
 
 psmdc.command = {
   a = { fg=c.grp.bg, bg=c.clr.cyan },
+  b = { fg=c.clr.cyan, bg=c.grp.surface_darker},
 }
 
 psmdc.visual = {
   a = { fg=c.grp.bg, bg=c.clr.lightyellow },
+  b = { fg=c.clr.lightyellow, bg=c.grp.surface_darker},
 }
 
 psmdc.replace = {
   a = { fg=c.grp.bg, bg=c.clr.orange },
+  b = { fg=c.clr.orange, bg=c.grp.surface_darker},
 }
 
 psmdc.inactive = {
