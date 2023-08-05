8b6342722a1bfff268ef148acd84417189327e62
Pranjal Soni <soni.pranjals17@gmail.com>
2023-08-05 10:44:41 +0530

    Remove dark variant
    It was uselss. I'm never going to use dark instead of darker.

--- colors/psmdc_dark.vim
+++
@@ -1,6 +0,0 @@
-" clear cache so this reloads changes; useful for development
-" lua package.loaded["psmdc"] = nil
-" lua package.loaded["psmdc.colors"] = nil
-" lua package.loaded["psmdc.utils"] = nil
-lua vim.opt.background = "dark"
-lua require("psmdc").colorscheme("dark")
--- lua/psmdc/colors.lua
+++ lua/psmdc/colors.lua
@@ -2,41 +2,6 @@ local M = {}
 local utils = require("psmdc.utils")
 local hsl = require("psmdc.hsluv").hsluv_to_hex
 
-M.dark = {}
-M.dark.clr = {
-  white  = hsl {0, 0, 100},
-  black  = hsl {0, 0, 0},
-  gray   = hsl {0, 0, 55},
-  red    = hsl {5, 100, 60},
-  orange = hsl {14, 88, 64.5},
-  yellow = hsl {57, 100, 84},
-  green  = hsl {102, 59, 88},
-  green2 = hsl {90, 80, 90},
-  cyan   = hsl {224, 99, 84},
-  blue   = hsl {265, 99, 69},
-  brown  = hsl {22, 37, 59},
-  purple = hsl {288, 75,  68},
-  pink   = hsl {337, 82, 64},
-  violet = hsl {314, 34, 61},
-
-  lightorange = hsl {21, 88, 66.5},
-  lightpink   = hsl {303, 80,  73},
-  lightblue   = hsl {217, 24, 80},
-  lightyellow = hsl {70, 80, 80},
-}
-M.dark.grp = {
-  bg               = hsl {285, 10, 5},
-  fg               = hsl {256, 12, 64},
-  surface          = hsl {285, 28, 13},
-  surface_darker   = hsl {285, 30, 9},
-  surfacefg        = hsl {256, 12, 64},
-  surfacefg_darker = hsl {256, 12, 64},
-  selection        = hsl {285, 28, 13},
-  comments         = hsl {256, 21, 37},
-  nontext          = hsl {258, 21, 26},
-  cursor_line      = hsl {285, 0, 2},
-}
-
 local hsluv = require("psmdc.hsluv").hsluv_to_hex
 
 M.darker = {}
