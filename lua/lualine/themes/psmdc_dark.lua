1fc79dd2480ec3de6bd55e4f469d79ecfc06ec1f
Pranjal Soni <soni.pranjals17@gmail.com>
2026-05-16 14:14:44 +0530

    lualine: change theme according to lualine components I use

--- lua/lualine/themes/psmdc_dark.lua
+++ lua/lualine/themes/psmdc_dark.lua
@@ -1,31 +1,32 @@
 local c = require("psmdc.colors").dark
+local blend = require("psmdc.utils").blend
 
 local psmdc = {}
 
 psmdc.normal = {
 	a = { fg = c.bg, bg = c.purple },
-	b = { fg = c.purple, bg = c.surface_darker },
-	c = { fg = c.surfacefg, bg = c.surface },
+	b = { bg = blend(c.purple, c.surface, 0.1) },
+	c = { fg = c.surfacefg, bg = c.bg },
 }
 
 psmdc.insert = {
 	a = { fg = c.bg, bg = c.pink },
-	b = { fg = c.pink, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.pink, c.surface, 0.1) },
 }
 
 psmdc.command = {
 	a = { fg = c.bg, bg = c.cyan },
-	b = { fg = c.cyan, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.cyan, c.surface, 0.1) },
 }
 
 psmdc.visual = {
 	a = { fg = c.bg, bg = c.yellow },
-	b = { fg = c.yellow, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.yellow, c.surface, 0.1) },
 }
 
 psmdc.replace = {
 	a = { fg = c.bg, bg = c.orange },
-	b = { fg = c.orange, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.orange, c.surface, 0.1) },
 }
 
 psmdc.inactive = {
