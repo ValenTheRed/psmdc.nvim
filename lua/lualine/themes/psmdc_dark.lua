739dab0e73b2f6ea31c43a6a7d0bc573248e84cf
Pranjal Soni <soni.pranjals17@gmail.com>
2026-05-16 14:09:31 +0530

    lualine: change dark theme

--- lua/lualine/themes/psmdc_dark.lua
+++ lua/lualine/themes/psmdc_dark.lua
@@ -1,35 +1,36 @@
 local c = require("psmdc.colors").dark
+local blend = require("psmdc.utils").blend
 
 local psmdc = {}
 
 psmdc.normal = {
 	a = { fg = c.bg, bg = c.func },
-	b = { fg = c.func, bg = c.surface_darker },
-	c = { fg = c.surfacefg, bg = c.surface },
+	b = { bg = blend(c.func, c.surface, 0.1) },
+	c = { fg = c.surfacefg, bg = c.bg },
 }
 
 psmdc.insert = {
 	a = { fg = c.bg, bg = c.menu_sel },
-	b = { fg = c.menu_sel, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.menu_sel, c.surface, 0.1) },
 }
 
 psmdc.command = {
 	a = { fg = c.bg, bg = c.delimiter },
-	b = { fg = c.delimiter, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.delimiter, c.surface, 0.1) },
 }
 
 psmdc.visual = {
 	a = { fg = c.bg, bg = c.type },
-	b = { fg = c.type, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.type, c.surface, 0.1) },
 }
 
 psmdc.replace = {
 	a = { fg = c.bg, bg = c.constant },
-	b = { fg = c.constant, bg = c.surface_darker },
+	b = { fg = c.fg, bg = blend(c.constant, c.surface, 0.1) },
 }
 
 psmdc.inactive = {
-	a = { bg = c.surface_lighter },
+	a = { fg = c.fg_4, bg = c.surface_lighter, },
 	b = "Normal",
 	c = "Normal",
 }
